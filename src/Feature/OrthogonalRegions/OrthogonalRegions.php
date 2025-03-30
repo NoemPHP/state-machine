eee6d174c88e8f19d4f97e3a8cd02858cdb3ed9f
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2025-03-30 12:48:00 +0200

    🎨 Add `Expect` import and enhance schema linking logic in OrthogonalRegions
    The commit introduces the use of `Nette\Schema\Expect` to improve the type definitions within the schema linking process. This update ensures that the `states` property is correctly defined as a list, maintaining the expected structure and behavior of orthogonal regions. The changes are encapsulated within detailed comments explaining the rationale behind each modification, ensuring clarity and maintainability of the code.

--- src/Feature/OrthogonalRegions/OrthogonalRegions.php
+++ src/Feature/OrthogonalRegions/OrthogonalRegions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Noem\State\Feature\OrthogonalRegions;
 
 use Nette\Schema\Elements\Type;
+use Nette\Schema\Expect;
 use Noem\State\Chains;
 use Noem\State\Feature\Feature;
 use Noem\State\Feature\Loader\LoaderChains;
@@ -14,6 +15,7 @@ use Noem\State\Middleware\ChainMail;
 
 class OrthogonalRegions implements Feature
 {
+
     public function __invoke(ChainMail $chainMail): void
     {
         $chainMail->supply(
@@ -31,10 +33,21 @@ class OrthogonalRegions implements Feature
                  */
                 $schema?->link(function (SchemaContext $context, callable $next) {
                     $nestedRegion = new Type('list');
-                    $context->state->extend([
+                    /**
+                     * The extend() method produces a new object
+                     */
+                    $context->state = $context->state->extend([
                         'regions' => $nestedRegion,
                     ]);
+                    /**
+                     * Therefore we need to update the existing reference to the states definition
+                     */
+                    $context->region = $context->region->extend([
+                        'states' => Expect::listOf($context->state),
+                    ]);
                     $nestedRegion->items($context->region);
+
+                    return $next($context);
                 });
                 //$get->link(
                 //    function (Chains\Context\GetContext $context, callable $next, callable $first) use ($parentRegion) {
