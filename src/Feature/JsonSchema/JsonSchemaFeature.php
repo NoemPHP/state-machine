3f164ca1c79e39d9ace03cd7340e7736cee0b151
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2025-04-05 00:17:05 +0200

    ✨ Enhance JsonSchemaFeature to support schema defaults and additional metadata processing.
    This commit introduces enhancements to the `JsonSchemaFeature` by adding support for setting up schema defaults through builder chains and processing additional metadata. The changes include updating the method signature to handle an additional parameter and implementing logic to modify the region builder based on the provided schema.

--- src/Feature/JsonSchema/JsonSchemaFeature.php
+++ src/Feature/JsonSchema/JsonSchemaFeature.php
@@ -6,19 +6,23 @@ namespace Noem\State\Feature\JsonSchema;
 
 use Nette\Schema\Elements\Structure;
 use Nette\Schema\Expect;
+use Noem\State\Chains\Meta;
+use Noem\State\Feature\ExtendedState\ContextMetaType;
 use Noem\State\Feature\Feature;
 use Noem\State\Feature\Loader\LoaderChains\Context\SchemaContext;
 use Noem\State\Middleware\ChainMail;
 use Noem\State\Feature\Loader\LoaderChains;
+use Noem\State\RegionBuilder;
 
 class JsonSchemaFeature implements Feature
 {
+
     public function __invoke(ChainMail $chainMail): void
     {
-        $chainMail->supply(
-        )->use(
+        $chainMail->supply()->use(
             function (
                 ?LoaderChains\Schema $schema,
+                ?LoaderChains\Loader $loader
             ) {
                 /**
                  * Extend the region schema to support the 'regions' item within a state config
@@ -47,6 +51,30 @@ class JsonSchemaFeature implements Feature
                     ]);
 
                     return $next($context);
+                });
+                /**
+                 * Setup schema defaults through the builder chains
+                 */
+                $loader?->link(function (LoaderChains\Context\LoaderContext $context, callable $next) {
+                    $data = $context->data;
+                    if (!isset($data['context']['schema'])) {
+                        return $next($context);
+                    }
+                    $schema = $data['context']['schema'];
+                    $builder = $next($context);
+                    assert($builder instanceof RegionBuilder);
+                    $builder->addStep(function (RegionBuilder $builder, callable $next) use ($schema) {
+                        $meta = $builder->chainMail->get(Meta::class);
+                        $region = $next($builder);
+                        $metadata = $meta->call(new \Noem\State\Chains\Params\Meta($region, ContextMetaType::get()));
+                        foreach ($schema as $type) {
+                            $metadata[$type['name']] = $type['default'] ?? null;
+                        }
+
+                        return $region;
+                    });
+
+                    return $builder;
                 });
             }
         );
