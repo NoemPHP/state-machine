1b18a01c23d814ca7d8671ee32941d52626c8d47
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2025-08-22 19:41:03 +0200

    🌟 Add `ToolSelect` class for AI chain feature. WIP

---
+++ src/Feature/Ai/Chains/ToolSelect.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Noem\State\Feature\Ai\Chains;
+
+use Noem\State\Feature\Ai\Chains\Params\SystemPromptParams;
+use Noem\State\Middleware\Chain;
+
+/**
+ * @template-extends Chain<SystemPromptParams,string>
+ */
+class ToolSelect extends Chain
+{
+}
