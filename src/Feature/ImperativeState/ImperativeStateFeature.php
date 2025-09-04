e47dffed1db08b32ef381f3dbf3e0ca883cd9314
Moritz Meißelbach <arbelzapf@gmail.com>
2025-09-04 22:07:44 +0200

    🎨 feature(api): add imperative state feature class.

---
+++ src/Feature/ImperativeState/ImperativeStateFeature.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Noem\State\Feature\ImperativeState;
+
+use Noem\State\Feature\Feature;
+use Noem\State\Middleware\ChainMail;
+
+class ImperativeStateFeature implements Feature
+{
+
+    public function __invoke(ChainMail $chainMail): void
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
